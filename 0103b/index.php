3097e9942c7b50f9725f2821fe394b61992dced3
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-04-21 16:22:05 +0200

    0103

---
+++ 0103b/index.php
@@ -0,0 +1,37 @@
+<!-- <style>
+    .coin {
+        background: #999999;
+        color: #333333;
+        border-radius: 50%;
+        padding: 50px;
+        text-align: center;
+        font-size: 2rem;
+        font-weight: bold;
+        width: 50px;
+    }
+</style> -->
+
+<?php
+
+function flip() {
+    return (0 == rand(0,1)) ? 'H' : 'T';
+}
+
+
+$H = 0;
+$T = 0;
+
+for( $i = 0; $i < 1000; $i++) {
+    $flip = flip();
+    ${$flip}++;
+}
+
+echo '<p>Heads was flipped ' . $H/10 . '% of the time. Tails was flipped ' . $T/10 . '%.';
+    
+    
+    
+?>
+    
+<!-- <div class='coin'>
+    <?php echo flip(); ?>
+</div> -->
