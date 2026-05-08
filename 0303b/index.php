d5cb50ced961dec406e45bf95a3ebe682f827ee3
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-05-08 22:13:44 +0200

    0303

--- 0303b/index.php
+++ 0303b/index.php
@@ -18,6 +18,72 @@ The table we’re working with is called “speakers”
         <meta name="author" value="Joe Casabona" />
     </head>
     <body>
-        <!--Content goes here-->
+        <main>
+            <h1>Speakers and Sessions</h1>
+            <table>
+			<thead>
+				<td>Name</td>
+				<td>Bio</td>
+				<td>Session Title</td>
+				<td>Session Desc</td>
+				<td>Track</td>
+			</thead>
+            <tbody>
+                <?php
+                $host = '127.0.01';
+                $user = 'mariadb';
+                $pass = 'mariadb';
+                $db = 'mariadb';
+
+                $mysqli = new mysqli($host, $user, $pass, $db);
+                $results = $mysqli->query('SELECT * FROM speakers;');
+
+                $format = '<tr>
+                        <td>%1$s</td>
+                        <td>%2$s</td>
+                        <td>%3$s</td>
+                        <td>%4$s</td>
+                        <td>%5$s</td>       
+                </tr>';
+
+                foreach($results as $row) {
+                    printf($format,
+                        $row['name'],
+                        $row['bio'],
+                        $row['session_name'],
+                        $row['session_desc'],
+                        $row['track']
+                    );
+                }
+                ?>
+                </tbody>
+            </table>
+        </main>
+        <style>
+		body {
+			background: #edf2f8;
+			font-family: 'Inter', sans-serf;
+			font-size: 18px;
+		}
+
+		main {
+			background: #FFFFFF;
+			padding: 40px;
+			margin: 0 auto;
+			width: 1000px;
+		}
+
+		table,
+		td {
+			border: 1px solid #CFCFCF;
+			padding: 5px;
+		}
+
+		.label {
+			color: #FFFFFF;
+			background: #880000;
+			padding: 5px;
+		}
+	    </style>
     </body>
 </html>
